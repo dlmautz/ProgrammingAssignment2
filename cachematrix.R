1640de3e3c379ec54b98312ecf28c556ed5a7f4e
dlmautz <dlmautz@gmail.com>
2015-04-20 00:21:30 -0400

    Mimic example + update random function
    Random function now checks determinant to ensure resulting matrix will
    be invertable.

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,15 @@
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  xInv <- NULL
+  set <- function(y) {
+    x <<- y
+    xInv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inv) xInv <<- inv
+  getInv <- function() xInv
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 ## cacheSolve:
@@ -13,8 +21,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should
 ## retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix()) {
+  xInv <- x$getInv()
+  if(!is.null(xInv)) {
+    message("getting cached data")
+    return(xInv)
+  }
+  xInv <- solve(x$get())
+  x$setInv(xInv)
+  xInv
 }
 
 ## makeRandomSquareMatrix:
@@ -22,7 +37,10 @@ cacheSolve <- function(x, ...) {
 ## makeCacheMatrix and cacheSolve functions above.
 
 makeRandomSquareMatrix <- function(n=3) {
-  if(n>10) {n<-10}
-  x <- matrix(sample(1:100, n^2, replace=TRUE),nrow=n,ncol=n)
+  if(n>10) n<-10
+  repeat {
+    x <- matrix(sample(1:100, n^2, replace=TRUE),nrow=n,ncol=n)
+    if(det(x)!=0) break
+  }
   x
 }
