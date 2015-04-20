b8b37856f6011ea43ab18b7b55e80c6ea4ff547a
dlmautz <dlmautz@gmail.com>
2015-04-19 23:31:59 -0400

    Added random matrix function
    Function will be used to generate matrices for testing purposes.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Caching Functions for Coursera R Programming - Assignment 2
+## Daniel Mautz - April 2015
 
-## Write a short comment describing this function
+## makeCacheMatrix:
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
+## cacheSolve:
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should
+## retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+
+## makeRandomSquareMatrix:
+## Creates a square matrix with size based on sole argument, to be used when testing
+## makeCacheMatrix and cacheSolve functions above.
+
+makeRandomSquareMatrix <- function(n=3) {
+  if(n>10) {n<-10}
+  x <- matrix(sample(1:100, n^2, replace=TRUE),nrow=n,ncol=n)
+  x
 }
